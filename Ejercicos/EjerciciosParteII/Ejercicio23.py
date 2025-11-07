634b4d7cc6b8b563ae695d0dbe863e30ac5256d5
RoscarinaW <oscarinawrn@gmail.com>
2025-11-07 16:25:36 -0500

    Actualizacion del ejercicios

--- Ejercicos/EjerciciosParteII/Ejercicio23.py
+++ Ejercicos/EjerciciosParteII/Ejercicio23.py
@@ -1,36 +1,29 @@
 #Programa para determinar si un deportista es aceptado o no en un equipo deportivo.
-
 print("Programa de ingreso al equipo")
 
 nombre = input("Ingrese el nombre del estudiante: ")
-nota1 = float(input("Ingrese la primera nota: "))
-nota2 = float(input("Ingrese la segunda nota: "))
-nota3 = float(input("Ingrese la tercera nota: "))
 fechaNacimiento = int(input("Ingrese el año de nacimiente del estudiante: "))
 fechaActual = int(input("Ingrese el año actual: "))
+estarura = float(input("Ingrese la estarura actual del estudiante: "))
+peso = float(input("Ingrese el peso actual del estudiante: "))
 
-promedio = (nota1 + nota2 + nota3) / 3
 edad = fechaActual - fechaNacimiento
 
-if promedio >= 3.0:
+if edad <= 18 and estarura <= 1.80 and peso <= 80:
     print("El estudiante {}".format(nombre))
     print("Edad del estudiante: {}".format(edad))
-    print("Nota #1: {}".format(nota1))
-    print("Nota #2: {}".format(nota2))
-    print("Nota #3: {}".format(nota3))
-    print(f"El estudiante {nombre} obtuvo un promedio: {promedio}")
-    print("Obtuvo nota APROBATORIA y entra al equipo")
+    print("La estatura actual del estudiantes es: {}".format(estarura))
+    print("El peso actual del estudiante es: {}".format(peso))
+    print("Obtuvo todas las condiciones APROBATORIA y entra al equipo")
     print("<<>>"*3)
     print("<<<FELICIDADES>>>")
     print("<<>>"*3)
 else:
     print("El estudiante {}".format(nombre))
     print("Edad del estudiante: {}".format(edad))
-    print("Nota #1: {}".format(nota1))
-    print("Nota #2: {}".format(nota2))
-    print("Nota #3: {}".format(nota3))
-    print(f"El estudiante {nombre} obtuvo un promedio: {promedio}")
-    print("Obtuvo nota REPROBATORIA y NO entra al equipo")
+    print("La estatura actual del estudiantes es: {}".format(estarura))
+    print("El peso actual del estudiante es: {}".format(peso))
+    print("No cumple con todas las condiciones, el estudiante NO ENTRA AL EQUIPO")
     print("<<>>"*3)
-    print("<<<SUERTE PARA LA PROXIMA>>>")
+    print("<<<Buena sueter>>>")
     print("<<>>"*3)
